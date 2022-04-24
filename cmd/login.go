9847a7eb3d821665225154cd4c9ce3e4cb5a9b5f
Jinquan Wang <w1637894214@163.com>
2022-04-24 15:58:57 +0800

    ✨ feat: 添加登录命令

---
+++ cmd/login.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/wangjq4214/buaa-login/internal"
+)
+
+var (
+	username string
+	password string
+	retry    int
+)
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Login BUAA gateway.",
+	Run: func(cmd *cobra.Command, args []string) {
+		lm := internal.NewLoginManager(internal.NewLoginManagerParams{
+			Username:           username,
+			Password:           password,
+			N:                  "200",
+			AcID:               "1",
+			Enc:                "srun_bx1",
+			VType:              "1",
+			LoginPageURL:       "https://gw.buaa.edu.cn/srun_portal_pc?ac_id=1&theme=buaa",
+			GetChallengeApiURL: "http://gw.buaa.edu.cn/cgi-bin/get_challenge",
+			LoginApiURL:        "http://gw.buaa.edu.cn/cgi-bin/srun_portal",
+		})
+
+		for i := 0; i < retry; i++ {
+			err := lm.Login()
+			if err != nil {
+				fmt.Println("We got an error while login:")
+				fmt.Println(err.Error())
+			} else {
+				os.Exit(0)
+			}
+		}
+	},
+}
+
+func init() {
+	loginCmd.Flags().StringVarP(&username, "username", "u", "", "Your buaa gw username.")
+	loginCmd.MarkFlagRequired("username")
+
+	loginCmd.Flags().StringVarP(&password, "password", "p", "", "Your buaa gw password.")
+	loginCmd.MarkFlagRequired("password")
+
+	loginCmd.Flags().IntVarP(&retry, "retry", "r", 5, "The login retry times.")
+
+	rootCmd.AddCommand(loginCmd)
+}
---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "0.0.1"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display the program version.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v" + Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
