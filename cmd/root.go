678d06940099236c075c38744b1f9a4574bd62c0
Jinquan Wang <w1637894214@163.com>
2022-04-24 15:38:56 +0800

    ✨ feat: 添加一个根命令

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,26 @@
-package main
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "buaa-login",
+	Short: "Use the command line to login the buaa campus network.",
+	Long: `Use the command line to login the buaa campus network.
+It mainly needs to implement several encryption algorithms,
+and obtain the current IP and Token through the API.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/wangjq4214/buaa-login/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
