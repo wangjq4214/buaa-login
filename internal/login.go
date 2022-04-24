3372261f18baf6e01e4d1c310947b76005cd351c
Jinquan Wang <w1637894214@163.com>
2022-04-24 15:23:11 +0800

    ✨ feat: 完成登录

--- internal/login.go
+++ internal/login.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
+	"strings"
+
+	"github.com/wangjq4214/buaa-login/internal/encryption"
 )
 
 var header = map[string]string{
@@ -16,13 +19,33 @@ var header = map[string]string{
 var (
 	ErrMatchIPFail    = errors.New("Can not match ip from page")
 	ErrMatchTokenFail = errors.New("Can not match token from api")
+	ErrLoginFail      = errors.New("Can not login from api")
 )
+
+func getRequestURL(s string, p map[string]string) (string, error) {
+	params := url.Values{}
+	parseURL, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+	for k, v := range p {
+		params.Set(k, v)
+	}
+	parseURL.RawQuery = params.Encode()
+
+	return parseURL.String(), nil
+}
 
 type LoginManager struct {
 	username           string
 	password           string
+	n                  string
+	acID               string
+	enc                string
+	vType              string
 	loginPageURL       string
 	getChallengeApiURL string
+	loginApiURL        string
 
 	ip    string
 	token string
@@ -33,17 +56,27 @@ type LoginManager struct {
 type NewLoginManagerParams struct {
 	Username string
 	Password string
+	N        string
+	AcID     string
+	Enc      string
+	VType    string
 
 	LoginPageURL       string
 	GetChallengeApiURL string
+	LoginApiURL        string
 }
 
 func NewLoginManager(params NewLoginManagerParams) *LoginManager {
 	return &LoginManager{
 		username:           params.Username,
 		password:           params.Password,
+		n:                  params.N,
+		acID:               params.AcID,
+		enc:                params.Enc,
+		vType:              params.VType,
 		loginPageURL:       params.LoginPageURL,
 		getChallengeApiURL: params.GetChallengeApiURL,
+		loginApiURL:        params.LoginApiURL,
 		client:             &http.Client{},
 	}
 }
@@ -86,17 +119,12 @@ func (l *LoginManager) getToken() error {
 	log.Println("Step2: Get token by resolving challenge result.")
 
 	// Get challenge
-	params := url.Values{}
-	parseURL, err := url.Parse(l.getChallengeApiURL)
-	if err != nil {
-		return err
-	}
-	params.Set("callback", "jQuery15815616146")
-	params.Set("username", l.username)
-	params.Set("ip", l.ip)
-	parseURL.RawQuery = params.Encode()
-
-	req, err := http.NewRequest(http.MethodGet, parseURL.String(), nil)
+	u, err := getRequestURL(l.getChallengeApiURL, map[string]string{
+		"callback": "jQuery15815616146",
+		"username": l.username,
+		"ip":       l.ip,
+	})
+	req, err := http.NewRequest(http.MethodGet, u, nil)
 	if err != nil {
 		return err
 	}
@@ -117,13 +145,122 @@ func (l *LoginManager) getToken() error {
 	// Get token from challenge api with regex
 	r := regexp.MustCompile("\"challenge\":\"(.*?)\"")
 	match := r.FindAllSubmatch(body, -1)
-	log.Printf("%q\n", match)
 	if len(match) == 0 {
 		return ErrMatchTokenFail
 	}
 	l.token = string(match[0][1])
 
 	log.Println("The token is: ", l.token)
+	log.Println("----------------")
+	return nil
+}
+
+func (l *LoginManager) encryptInfo() (string, error) {
+	info := [][]string{
+		{"username", l.username},
+		{"password", l.password},
+		{"ip", l.ip},
+		{"acid", l.acID},
+		{"enc_ver", l.enc},
+	}
+
+	infoLength := len(info)
+	builder := strings.Builder{}
+	builder.WriteString("{")
+	for i, v := range info {
+		builder.WriteString("\"")
+		builder.WriteString(strings.Trim(v[0], " "))
+		builder.WriteString("\":\"")
+		builder.WriteString(strings.Trim(v[1], " "))
+		builder.WriteString("\"")
+		if i < infoLength-1 {
+			builder.WriteString(",")
+		}
+	}
+	builder.WriteString("}")
+
+	return "{SRBX1}" + encryption.GetBase64(encryption.GetXencode(builder.String(), l.token)), nil
+}
+
+func (l *LoginManager) encryptMD5() string {
+	return "{MD5}" + encryption.GetMD5(l.token, "")
+}
+
+func (l *LoginManager) encryptChecksum(md5, info string) string {
+	builder := strings.Builder{}
+
+	builder.WriteString(l.token)
+	builder.WriteString(l.username)
+
+	builder.WriteString(l.token)
+	builder.WriteString(encryption.GetMD5(l.token, ""))
+
+	builder.WriteString(l.token)
+	builder.WriteString(l.acID)
+
+	builder.WriteString(l.token)
+	builder.WriteString(l.ip)
+
+	builder.WriteString(l.token)
+	builder.WriteString(l.n)
+
+	builder.WriteString(l.token)
+	builder.WriteString(l.vType)
+
+	builder.WriteString(l.token)
+	builder.WriteString(info)
+
+	return encryption.GetSHA1(builder.String())
+}
+
+func (l *LoginManager) login() error {
+	log.Println("Step3: Login and resolve response.")
+	info, err := l.encryptInfo()
+	if err != nil {
+		return err
+	}
+	md5 := l.encryptMD5()
+	checksum := l.encryptChecksum(md5, info)
+
+	u, err := getRequestURL(l.loginApiURL, map[string]string{
+		"callback": "jsonp1583251661368",
+		"action":   "login",
+		"username": l.username,
+		"password": md5,
+		"ac_id":    l.acID,
+		"ip":       l.ip,
+		"info":     info,
+		"chksum":   checksum,
+		"n":        l.n,
+		"type":     l.vType,
+	})
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return err
+	}
+	for k, v := range header {
+		req.Header.Add(k, v)
+	}
+	response, err := l.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println(string(body))
+
+	r := regexp.MustCompile("\"suc_msg\":\"(.*?)\"")
+	match := r.FindAllSubmatch(body, -1)
+	if len(match) == 0 {
+		return ErrLoginFail
+	}
+
+	log.Println("The success msg is: ", string(match[0][1]))
 	log.Println("----------------")
 	return nil
 }
@@ -135,6 +272,11 @@ func (l *LoginManager) Login() error {
 	}
 
 	err = l.getToken()
+	if err != nil {
+		return err
+	}
+
+	err = l.login()
 	if err != nil {
 		return err
 	}
--- internal/login_test.go
+++ internal/login_test.go
@@ -8,10 +8,15 @@ import (
 
 func TestLogin(t *testing.T) {
 	lm := internal.NewLoginManager(internal.NewLoginManagerParams{
-		Username:           "BY2106105",
+		Username:           "by2106105",
 		Password:           "",
+		N:                  "200",
+		AcID:               "1",
+		Enc:                "srun_bx1",
+		VType:              "1",
 		LoginPageURL:       "https://gw.buaa.edu.cn/srun_portal_pc?ac_id=1&theme=buaa",
 		GetChallengeApiURL: "http://gw.buaa.edu.cn/cgi-bin/get_challenge",
+		LoginApiURL:        "http://gw.buaa.edu.cn/cgi-bin/srun_portal",
 	})
 
 	err := lm.Login()
